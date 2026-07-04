61abca7c6926d3da4ed890c2f61e780690c64698
MadeUp MonkeyShit <tricorpglobal@gmail.com>
2026-07-04 13:52:40 -0700

    Update admin-albums-query.js

--- functions/admin-albums-query.js
+++ functions/admin-albums-query.js
@@ -17,7 +17,8 @@ export async function onRequestGet(context) {
                 youtube_music, spotify_streams, apple_streams, amazon_streams, tidal_streams,
                 tt_likes, fb_likes, in_likes, yt_likes,
                 tt_shares, fb_shares, in_shares, in_reposts,
-                tt_saves, fb_saves, in_saves, sp_saves, sp_listeners
+                tt_saves, fb_saves, in_saves, sp_saves, sp_listeners,
+                dk_total
          FROM song_stats`
       ).all();
       const totalsMap = buildTotalsMap(songs || []);
@@ -35,7 +36,8 @@ export async function onRequestGet(context) {
                 youtube_music, spotify_streams, apple_streams, amazon_streams, tidal_streams,
                 tt_likes, fb_likes, in_likes, yt_likes,
                 tt_shares, fb_shares, in_shares, in_reposts,
-                tt_saves, fb_saves, in_saves, sp_saves, sp_listeners
+                tt_saves, fb_saves, in_saves, sp_saves, sp_listeners,
+                dk_total
          FROM song_stats WHERE album_id=?`
       ).bind(id).all();
       return json({ success: true, result: { ...album, ...computeTotals(songs || []) } });
@@ -60,28 +62,30 @@ function buildTotalsMap(songs) {
     if (!map[s.album_id]) map[s.album_id] = zeroes();
     const t = map[s.album_id];
     t.song_count++;
-    t.total_streams += (s.tiktok_views||0)+(s.facebook_views||0)+(s.instagram_views||0)+(s.youtube_views||0)+(s.youtube_music||0)+(s.spotify_streams||0)+(s.apple_streams||0)+(s.amazon_streams||0)+(s.tidal_streams||0);
-    t.total_likes   += (s.tt_likes||0)+(s.fb_likes||0)+(s.in_likes||0)+(s.yt_likes||0);
-    t.total_shares  += (s.tt_shares||0)+(s.fb_shares||0)+(s.in_shares||0)+(s.in_reposts||0);
-    t.total_saves   += (s.tt_saves||0)+(s.fb_saves||0)+(s.in_saves||0)+(s.sp_saves||0);
-    t.total_listeners += (s.sp_listeners||0);
+    t.total_streams    += (s.tiktok_views||0)+(s.facebook_views||0)+(s.instagram_views||0)+(s.youtube_views||0)+(s.youtube_music||0)+(s.spotify_streams||0)+(s.apple_streams||0)+(s.amazon_streams||0)+(s.tidal_streams||0);
+    t.dk_total_streams += (s.dk_total||0);
+    t.total_likes      += (s.tt_likes||0)+(s.fb_likes||0)+(s.in_likes||0)+(s.yt_likes||0);
+    t.total_shares     += (s.tt_shares||0)+(s.fb_shares||0)+(s.in_shares||0)+(s.in_reposts||0);
+    t.total_saves      += (s.tt_saves||0)+(s.fb_saves||0)+(s.in_saves||0)+(s.sp_saves||0);
+    t.total_listeners  += (s.sp_listeners||0);
   }
   return map;
 }
 
 function computeTotals(songs) {
   return songs.reduce((t, s) => {
-    t.total_streams += (s.tiktok_views||0)+(s.facebook_views||0)+(s.instagram_views||0)+(s.youtube_views||0)+(s.youtube_music||0)+(s.spotify_streams||0)+(s.apple_streams||0)+(s.amazon_streams||0)+(s.tidal_streams||0);
-    t.total_likes   += (s.tt_likes||0)+(s.fb_likes||0)+(s.in_likes||0)+(s.yt_likes||0);
-    t.total_shares  += (s.tt_shares||0)+(s.fb_shares||0)+(s.in_shares||0)+(s.in_reposts||0);
-    t.total_saves   += (s.tt_saves||0)+(s.fb_saves||0)+(s.in_saves||0)+(s.sp_saves||0);
-    t.total_listeners += (s.sp_listeners||0);
+    t.total_streams    += (s.tiktok_views||0)+(s.facebook_views||0)+(s.instagram_views||0)+(s.youtube_views||0)+(s.youtube_music||0)+(s.spotify_streams||0)+(s.apple_streams||0)+(s.amazon_streams||0)+(s.tidal_streams||0);
+    t.dk_total_streams += (s.dk_total||0);
+    t.total_likes      += (s.tt_likes||0)+(s.fb_likes||0)+(s.in_likes||0)+(s.yt_likes||0);
+    t.total_shares     += (s.tt_shares||0)+(s.fb_shares||0)+(s.in_shares||0)+(s.in_reposts||0);
+    t.total_saves      += (s.tt_saves||0)+(s.fb_saves||0)+(s.in_saves||0)+(s.sp_saves||0);
+    t.total_listeners  += (s.sp_listeners||0);
     return t;
   }, zeroes());
 }
 
 function zeroes() {
-  return { total_streams:0, total_likes:0, total_shares:0, total_saves:0, total_listeners:0, song_count:0 };
+  return { total_streams:0, dk_total_streams:0, total_likes:0, total_shares:0, total_saves:0, total_listeners:0, song_count:0 };
 }
 
 function json(body, status = 200) {
